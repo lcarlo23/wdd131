7bc812bd04190161e9392e26a9a0925a58cee453
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2025-09-05 21:08:39 +0200

    Completed Dom Event Handling Activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,26 @@ const input = document.getElementById('favchap');
 const button = document.querySelector('button');
 const list = document.getElementById('list');
 
-const entry = document.createElement('li');
-const deleteBtn = document.createElement('button');
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const entry = document.createElement('li');
+        const deleteBtn = document.createElement('button');
 
-entry.textContent = input.value;
-deleteBtn.textContent = '❌';
+        entry.textContent = input.value;
+        deleteBtn.textContent = '❌';
 
-entry.append(deleteBtn);
-list.append(entry);
+        entry.append(deleteBtn);
+        list.append(entry);
+
+        deleteBtn.addEventListener('click', function () {
+            list.removeChild(entry);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+    } else {
+        input.focus();
+        return;
+    }
+})
