38c70b6f38924d3ae83ebf34448b1936661fdcc1
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2025-10-03 14:08:03 +0200

    Fixed Review Page Counter

--- scripts/review.js
+++ scripts/review.js
@@ -3,12 +3,10 @@ const reviewCounter = document.getElementById('review-counter');
 let count = localStorage.getItem('reviewsCount') || 0;
 
 document.addEventListener("DOMContentLoaded", () => {
-    if (window.location.pathname === '/review.html') {
-        count++;
-        localStorage.setItem('reviewsCount', count);
+    count++;
+    localStorage.setItem('reviewsCount', count);
 
-        const p = document.createElement('p');
-        p.textContent = `Reviews Completed: ${count}`;
-        reviewCounter.appendChild(p);
-    }
+    const p = document.createElement('p');
+    p.textContent = `Reviews Completed: ${count}`;
+    reviewCounter.appendChild(p);
 })
